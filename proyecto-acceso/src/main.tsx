2f49e0263b7a4b3b14394ef2d7d2e040044354fc
Ignacio Soto <ignaciosoto292@gmail.com>
2025-03-30 18:34:13 -0300

    creacion de rutas y ruta protegida

--- proyecto-acceso/src/main.tsx
+++ proyecto-acceso/src/main.tsx
@@ -3,12 +3,30 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Singup from './routes/singup.tsx'
+import Login from './routes/login.tsx'
+import Dashboard from './routes/dashboard.tsx'
+import ProtectedRoute from './routes/protectedRoute.tsx'
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Login />,
+  },
+  {
+    path: "/singup",
+    element: <Singup />,
+  },
+  {
+    path: "/",
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      }
+    ],
   },
 ]);
 
