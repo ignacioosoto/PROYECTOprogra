01f9a88c47945fb5d81dc4cf4ea23fdfbbe59ed1
Ignacio Soto <ignaciosoto292@gmail.com>
2025-04-19 13:28:23 -0400

    signup conectado con back

--- proyecto-acceso-back/routes/signout.js
+++ proyecto-acceso-back/routes/signout.js
@@ -2,22 +2,7 @@ const router = require("express").Router();
 
 //definimos la ruta de signout
 router.post("/", (req, res) => {
-    const { username, name, password } = req.body;
 
-    //por si se manda la solicitud de singout sin ningun campo
-    if (!!username || !!name || !!password) {
-        return res.status(400).json(
-            jsonResponse(400, {
-                error: "Fields are required",
-            })
-        )
-    }
-
-    //crear usuarui en la base de datos
-    res.status(200).json(jsonResponse(200, { message: "User created successfully" }))
-
-
-    res.send("signout");
 });
 
 module.exports = router;
