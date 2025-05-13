05124a18f1aa7e0eace14e42244c74389b52148f
Javier Abde <javierabde237@gmail.com>
2025-05-12 23:33:27 -0400

    registro de visitas
    registro de visitas

--- proyecto-acceso-front/src/routes/RVisitas.tsx
+++ proyecto-acceso-front/src/routes/RVisitas.tsx
@@ -1,8 +1,95 @@
-export default function RegisterVisit() {
-    return (
-      <div>
-        <h1>Registrar Visita</h1>
-        <p>Aquí puedes registrar una nueva visita.</p>
-      </div>
-    );
+import { useState } from "react";
+import DefaultLayout from "../layout/defaultLayout";
+import { API_URL } from "../auth/constants";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function VisitRegister() {
+  const [fullName, setFullName] = useState("");
+  const [company, setCompany] = useState("");
+  const [reason, setReason] = useState("");
+  const [idNumber, setIdNumber] = useState("");
+  const [errorResponse, setErrorResponse] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const response = await fetch(`${API_URL}/visits`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          fullName,
+          company,
+          reason,
+          idNumber,
+        }),
+      });
+
+      if (response.ok) {
+        setErrorResponse("");
+        toast.success("Visita registrada exitosamente");
+        setFullName("");
+        setCompany("");
+        setReason("");
+        setIdNumber("");
+      } else {
+        const json = await response.json();
+        setErrorResponse(json.body?.error || "Error desconocido");
+        toast.error(`Error: ${json.body?.error || "Error desconocido"}`);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Ocurrió un error al registrar la visita");
+    } finally {
+      setLoading(false);
+    }
   }
+
+  return (
+    <DefaultLayout>
+      <form onSubmit={handleSubmit}>
+        <h1>Registro de Visitas</h1>
+        {!!errorResponse && <div className="errorMessage">{errorResponse}</div>}
+        <label>Nombre Completo</label>
+        <input
+          type="text"
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+          required
+        />
+
+        <label>Rut</label>
+        <input
+          type="text"
+          value={company}
+          onChange={(e) => setCompany(e.target.value)}
+          required
+        />
+
+        <label>Motivo de la visita</label>
+        <input
+          type="text"
+          value={reason}
+          onChange={(e) => setReason(e.target.value)}
+          required
+        />
+
+        <label>Número de Identificación</label>
+        <input
+          type="text"
+          value={idNumber}
+          onChange={(e) => setIdNumber(e.target.value)}
+          required
+        />
+
+        <button type="submit" disabled={loading}>
+          {loading ? "Registrando..." : "Registrar Visita"}
+        </button>
+      </form>
+      <ToastContainer position="top-right" autoClose={3000} />
+    </DefaultLayout>
+  );
+}
