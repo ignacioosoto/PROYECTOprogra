ba48b1121aa6f694fdf1014845ed61c1a938beb5
Ignacio Soto <ignaciosoto292@gmail.com>
2025-06-08 20:52:20 -0400

    nuevo_commit

--- proyecto-acceso-front/src/routes/dashboard.tsx
+++ proyecto-acceso-front/src/routes/dashboard.tsx
@@ -77,18 +77,12 @@ export default function AuthLayout({ children }: DefaultLayoutProps) {
         <div className="grid grid-cols-2 gap-4 mt-6 w-full max-w-md px-4">
           <Link to="/signup" className="button-home">Agregar Administrador</Link>
           <Link to="/newowner" className="button-home">Agregar Propietarios</Link>
-<<<<<<< HEAD
           <Link to="/addbuilding" className="button-home">Agregar Edificio</Link>
-=======
->>>>>>> fcd6949c6522205839f41969179546eabc6a78ee
           <Link to="/dynamic-qr" className="button-home">QR Dinámico</Link>
           <Link to="/verificar" className="button-home">Access Point</Link>
           <Link to="/RVisitas" className="button-home">Registrar Visita</Link>
         </div>
-<<<<<<< HEAD
 
-=======
->>>>>>> fcd6949c6522205839f41969179546eabc6a78ee
 
       </div>
 
